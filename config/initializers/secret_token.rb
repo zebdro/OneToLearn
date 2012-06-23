38b20bd96a0ce9994e068afc2b3a19741999bb80
Zeb Dropkin <zeb.dropkin@gmail.com>
2012-06-23 14:24:04 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Onetolearn::Application.config.secret_token = '6b8164f72109f36280f7c69b9ee64d386cc6712879fd053ef613c976e982ba7b80de6b963820bca4d75a77035bb7c1f3baa57c51a96531d5acd085b5c9d4eb14'
